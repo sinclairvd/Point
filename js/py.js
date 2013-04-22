dc4ec9c1b9d87ec6eea636f3e5128921a9cfe6e5
Sinclair <ict@maasbach.com>
2013-04-22 12:09:46 +0200

    tryout new internet check code

--- js/py.js
+++ js/py.js
@@ -334,3 +334,36 @@ function handleV2020RandomForm(e) {
 	//alert("just prevented default post");
     
 };
+
+function checkConnection() {
+        var networkState = navigator.network.connection.type;
+ 
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.NONE]     = 'No network connection';
+ 
+        alert('Connection type: '+ states[networkState]);
+}
+
+function onOffline() {
+    // Handle the offline event
+    alert("You are offline!");
+    //var offline = $('#offline');
+    //var online = $('#online');
+    //offline.show();
+    //online.hide();       
+}
+
+function onOnline() {
+    // Handle the online event
+    alert("You are online!");
+    //var online = $('#online');
+    //var offline = $('#offline');
+    //online.show();
+    //offline.hide(); 
+}
